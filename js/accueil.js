fda8f08b653c95b35bc3c5435a998c64e7bcf582
Yann Fournier <y.fourni@icloud.com>
2024-06-10 12:23:08 +0200

    rework des templates

--- js/accueil.js
+++ js/accueil.js
@@ -1,6 +1,66 @@
-fetch('../html/header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header').innerHTML = data;
-    })
-    .catch(error => console.error('Error loading the HTML:', error));
+// Bouton
+var buLivres = document.getElementById('boutonLivres');
+var buAuteurs = document.getElementById('boutonAuteurs');
+var buUtilisateurs = document.getElementById('boutonUtilisateurs');
+var resultatsLivres = document.getElementsByClassName('buResultatLivre');
+var resultatsAuteurs = document.getElementsByClassName('buResultatAuteur');
+var resultatsProfils = document.getElementsByClassName('buResultatProfil');
+
+// Resultats
+var divLivres = document.getElementById('resultatsLivres');
+var divAuteurs = document.getElementById('resultatsAuteurs');
+var divUtilisateurs = document.getElementById('resultatsUtilisateurs');
+
+
+buLivres.addEventListener('click', function () {
+    buLivres.className = "selectBouton";
+    buAuteurs.className = "notSelectBouton";
+    buUtilisateurs.className = "notSelectBouton";
+
+    divLivres.className = "selectDiv";
+    divAuteurs.className = "notSelectDiv";
+    divUtilisateurs.className = "notSelectDiv";
+});
+
+buAuteurs.addEventListener('click', function () {
+    buLivres.className = "notSelectBouton";
+    buAuteurs.className = "selectBouton";
+    buUtilisateurs.className = "notSelectBouton";
+
+    divLivres.className = "notSelectDiv";
+    divAuteurs.className = "selectDiv";
+    divUtilisateurs.className = "notSelectDiv";
+});
+
+buUtilisateurs.addEventListener('click', function () {
+    buLivres.className = "notSelectBouton";
+    buAuteurs.className = "notSelectBouton";
+    buUtilisateurs.className = "selectBouton";
+
+    divLivres.className = "notSelectDiv";
+    divAuteurs.className = "notSelectDiv";
+    divUtilisateurs.className = "selectDiv";
+});
+
+// Ajout d'une action click pour tous les bouton resultats
+for (let element of resultatsLivres) {
+    element.addEventListener('click', function () {
+        var valeur = element.value;
+        console.log(valeur);
+        window.location.href = "livre.html?Id=" + encodeURIComponent(valeur);
+    });
+}
+for (let element of resultatsAuteurs) {
+    element.addEventListener('click', function () {
+        var valeur = element.value;
+        console.log(valeur);
+        window.location.href = "auteur.html?Id=" + encodeURIComponent(valeur);
+    });
+}
+for (let element of resultatsProfils) {
+    element.addEventListener('click', function () {
+        var valeur = element.value;
+        console.log(valeur);
+        window.location.href = "profil.html?Id=" + encodeURIComponent(valeur);
+    });
+}
--- js/header.js
+++ js/header.js
@@ -14,26 +14,52 @@ function isCookieSet(cookieName) {
     return false; // Le cookie n'est pas trouvé
 }
 
-// Exemple d'utilisation
-if (isCookieSet('myCookie')) {
-    console.log('Le cookie "myCookie" est défini.');
-} else {
-    console.log('Le cookie "myCookie" n\'est pas défini.');
-}
-
 function getCookieValue(cookieName) {
-        
+
     // Parcourir chaque cookie
     for (let i = 0; i < cookies.length; i++) {
         const cookie = cookies[i].trim();
-        
+
         // Vérifier si le cookie commence par le nom recherché suivi de "="
         if (cookie.startsWith(cookieName + '=')) {
             // Retourner la valeur du cookie en supprimant le "cookieName="
             return cookie.substring(cookieName.length + 1);
         }
     }
-    
+
     // Si le cookie n'est pas trouvé, retourner null ou une chaîne vide
     return null;
+}
+
+// Exemple d'utilisation
+if (isCookieSet('isConnected')) {
+    if (getCookieValue("isConnected") == "0") {
+        fetch('../html/notConnectedHeader.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('header').innerHTML = data;
+            })
+            .catch(error => console.error('Error loading the HTML:', error));
+    } else if (getCookieValue("isConnected") == "1") {
+        fetch('../html/connectedHeader.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading the HTML:', error));
+    } else if (getCookieValue("isConnected") == "2") {
+        fetch('../html/adminHeader.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading the HTML:', error));
+    }
+} else {
+    fetch('../html/notConnectedHeader.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading the HTML:', error));
 }
